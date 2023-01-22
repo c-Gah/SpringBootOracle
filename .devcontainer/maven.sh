d342bd4ef60db3b15cf6124126d38132147829cd
c-Gah <86792851+c-Gah@users.noreply.github.com>
2023-01-22 15:21:23 -0500

    added maven

---
+++ .devcontainer/maven.sh
@@ -0,0 +1,5 @@
+#export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export M2_HOME=/opt/maven
+export PATH=${M2_HOME}/bin:${PATH}
+export MAVEN_OPTS=-Xmx1024m
